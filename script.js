6335a4ab9801b1bf330252d42bbdb78b0255f0d1
PanchenkoNazar <120579167+PanchenkoNazar@users.noreply.github.com>
2023-06-27 16:38:52 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,22 @@
+// // 1.
+// let number = parseInt(prompt("Введите число:"));
+
+// if (number % 2 === 0) {
+//     console.log(alert("Число четное!"));
+// } else {
+//     console.log(alert("Число нечетное!"));
+// }
+
+
+// // 2.
+// let number = parseFloat(prompt("Введите число:"));
+
+// let text = (number >= 0) ? "Число положительное!" : "Число отрицательное!";
+// console.log(alert(text));
+
+// 3.
+let number = parseInt(prompt("Введите число:"));
+
+let text = (number % 2 === 0) ? "Число четное!" : "Число нечетное!";
+console.log(alert(text));
+1
